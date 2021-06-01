8f6a85d7f3c66c9682f473b85cd3e0471f6f5e5b
Anish Yadav <anishyadav60650@gmial.com>
2021-06-01 11:01:57 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-function compute() {
-    let principal = document.getElementById("principal").value;
-    let rate = document.getElementById("rate").value;
-    let years = document.getElementById("years").value;
-    let interest = principal * years * rate / 100;
-    let year = new Date().getFullYear() + parseInt(years);
-    let amount = parseInt(principal) + parseFloat(interest);
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year + "\<br\>"
+  function showMessage() {
+      document.getElementById("result").style.display = "block";
+  };
 
-}
+  function compute() {
+      var principal = document.getElementById("principal").value;
+      if (parseInt(principal) < 1) {
+          alert("Enter a positive number");
+          document.getElementById("principal").focus();
+          return;
+      }
 
-function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
-}
 
-function checkAmount() {
-    if (principal.value <= 0) { alert("Enter a positive number"); }
-}
+      var rate = document.getElementById("rate").value;
+      var years = document.getElementById("years").value;
+      var interest = principal * years * rate / 100;
+      var amount = parseFloat(principal) + parseFloat(interest);
+
+      var today = new Date();
+      var yearOfWithdrawal = parseInt(today.getFullYear()) + parseInt(years);
+
+      document.getElementById("depositmessage").innerHTML = "" + principal;
+      document.getElementById("interestmessage").innerHTML = "" + rate;
+      document.getElementById("amountmessage").innerHTML = "" + amount;
+      document.getElementById("yearmessage").innerHTML = "" + yearOfWithdrawal;
+      showMessage();
+
+      return false;
+  };
+
+  function showRange() {
+      var rate = document.getElementById("rate").value;
+      document.getElementById("ratevalue").innerHTML = rate;
+  }
